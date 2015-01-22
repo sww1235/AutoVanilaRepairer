f4e3719e26e07a00d88290b7cb905a4096b31c4d
sww1235 <sww1235@gmail.com>
2015-01-22 16:09:28 -0700

    Adding mcmod.info file

--- src/main/java/com/sww1235/AVR/AutoVanillaRepairer.java
+++ src/main/java/com/sww1235/AVR/AutoVanillaRepairer.java
@@ -1,12 +1,30 @@
 package com.sww1235.AVR;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-/**
- * Created by Stephen on 1/22/15.
- */
 @Mod(modid = "AVR", name = "Auto Vanila Repairer", version = "1.7.10-1.0")
 public class AutoVanillaRepairer
     {
+        @Mod.Instance("AVR")
+        public static AutoVanillaRepairer instance;
+        @Mod.EventHandler
+        public void preInit(FMLPreInitializationEvent event)
+            {
+
+            }
+        @Mod.EventHandler
+        public void Init(FMLInitializationEvent event)
+            {
+
+            }
+        @Mod.EventHandler
+        public void postInit(FMLPostInitializationEvent event)
+            {
+
+            }
+
 
     }
