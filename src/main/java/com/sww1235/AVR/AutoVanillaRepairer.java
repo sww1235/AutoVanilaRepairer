5f4b5251475f7edce5ca0b7a317a40566bc765d2
sww1235 <sww1235@gmail.com>
2015-01-22 02:15:49 -0700

    Third commit after integrating gradle with intelij

---
+++ src/main/java/com/sww1235/AVR/AutoVanillaRepairer.java
@@ -0,0 +1,12 @@
+package com.sww1235.AVR;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Stephen on 1/22/15.
+ */
+@Mod(modid = "AVR", name = "Auto Vanila Repairer", version = "1.7.10-1.0")
+public class AutoVanillaRepairer
+    {
+
+    }
